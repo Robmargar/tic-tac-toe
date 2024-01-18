472b74596239d268313fcc75dcdc2ea15e7f3633
Robmargar <roberto.mar.garc@gmail.com>
2024-01-18 16:15:00 -0600

    Aviso ya muestra icono de empate

--- src/Components/Aviso.jsx
+++ src/Components/Aviso.jsx
@@ -9,7 +9,9 @@ export const Aviso = ({ganador,reiniciar}) => {
        <div class='bg-slate-700 p-3 rounded flex flex-col justify-center items-center  gap-5 '>
         <h2 class="text-2xl text-gray-400">{textoGanador}</h2>
         <header class=" flex border-double border-4 border-sky-500 p-2 m-2 w-14 h-14 justify-center items-center">
-            {ganador && <span>{ganador}</span>}
+            { 
+            ganador !==false ? <span>{ganador}</span> : <span>🤝</span>
+            }
         </header>
         <footer>
             <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 w-full " onClick={reiniciar}>Empezar de nuevo</button>  
